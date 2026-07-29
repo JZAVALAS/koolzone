6c76bc0ac398f2fa5caf4c49a050a3d5f9d853a1
JZAVALAS <92447179+JZAVALAS@users.noreply.github.com>
2026-07-28 21:29:57 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -138,8 +138,8 @@ var PRODUCTOS = [
   },*/
    {
     nombre: "🔋Pack Baterías TELESIN p/ GoPro 9/10/11/12",
-    precio: "149.99",
-    precioAntes: "179.99",
+    precio: "129.99",
+    precioAntes: "159.99",
     categoria: "Cámara y Foto",
     imagenes: [
       "fotos/productos/57.bateria-gopro-hero-9-10-11-12.jpg",
